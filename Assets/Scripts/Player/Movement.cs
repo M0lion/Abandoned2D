698edcfe93d4f12b2d7491ddf3dcc5c1dc2e78ea
M0lion <bjorn-moholt@hotmail.com>
2015-10-23 19:12:45 +0200

    First

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    float speed = 5;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+        Ray mousePos = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 target = -(mousePos.origin + (mousePos.direction * (mousePos.origin.z / mousePos.direction.z)));
+           
+        //mousePos.z = mousePos.y;
+        //mousePos.y = 0;
+
+        Vector3 distance = target - transform.position;
+
+        Debug.DrawRay(mousePos.origin, mousePos.direction);
+        Debug.DrawLine(mousePos.origin, target, Color.red);
+
+        transform.rotation.SetAxisAngle(new Vector3(0, 0, 1), Vector3.Angle(distance, new Vector3(0, 1, 0)));
+
+        Debug.DrawRay(transform.position, distance);
+        Vector3 move = distance.normalized * speed * Time.deltaTime;
+
+        transform.position += move;
+	}
+}
---
+++ Assets/Scripts/Player/Triangle.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class Triangle : MonoBehaviour {
+
+    public MeshFilter meshFilter;
+
+    Texture2D tex;
+
+	void Start () {
+        Mesh mesh = meshFilter.mesh;
+        mesh.Clear();
+
+        tex = new Texture2D(1,1);
+        tex.SetPixel(0, 0, Color.white);
+
+        List<Vector3> vertices = new List<Vector3>();
+        vertices.Add(new Vector3(0, 0.5f, 0));
+        vertices.Add(new Vector3(0.5f, -0.5f, 0));
+        vertices.Add(new Vector3(-0.5f, -0.5f, 0));
+        //mesh.SetVertices(vertices); 
+        mesh.vertices = vertices.ToArray();
+
+        List<Vector2> uvs = new List<Vector2>();
+        uvs.Add(new Vector2(0, 0));
+        uvs.Add(new Vector2(0, 0));
+        uvs.Add(new Vector2(0, 0));
+        //mesh.SetUVs(0, uvs);
+        mesh.uv = uvs.ToArray();
+
+        Vector3[] normals = new Vector3[3];
+        normals[0] = new Vector3(0, 0, -1);
+        normals[1] = new Vector3(0, 0, -1);
+        normals[2] = new Vector3(0, 0, -1);
+        mesh.normals = normals;
+
+        List<int> triangles = new List<int>();
+        triangles.Add(0);
+        triangles.Add(1);
+        triangles.Add(2);
+        //mesh.SetTriangles(triangles, 0);
+        mesh.triangles = triangles.ToArray();
+    }
+	
+	void Update () {
+	
+	}
+}
