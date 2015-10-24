879dcf1c6885db6e1dfaee4d89c271fc35c758f8
M0lion <bjorn-moholt@hotmail.com>
2015-10-25 01:00:13 +0200

    Forcefield/lever

---
+++ Assets/Scripts/Objectives/Power/Lever.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lever : MonoBehaviour {
+
+    public Power target;
+
+    public MeshRenderer active;
+    public MeshRenderer inactive;
+    bool switchMeshes;
+
+    float cooldown = 0.5f;
+    float elapsedCooldown = 0;
+    bool coolingDown = false;
+
+    public bool powered = false;
+
+    void Start()
+    {
+        switchMeshes = active != null && inactive != null;
+    }
+
+    void Update()
+    {
+        if (coolingDown)
+        {
+            elapsedCooldown += Time.deltaTime;
+            if(elapsedCooldown >= cooldown)
+            {
+                coolingDown = false;
+                elapsedCooldown = 0;
+            }
+        }
+    }
+
+    void SwitchMeshes()
+    {
+        if (active != null)
+            active.enabled = powered;
+
+        if (inactive != null)
+            inactive.enabled = !powered;
+    }
+
+    public void Flip()
+    {
+        if (coolingDown)
+            return;
+
+        if (powered)
+            Off();
+        else
+            On();
+
+        transform.Rotate(Vector3.up, 180);
+        
+        coolingDown = true;
+    }
+
+    void On()
+    {
+        powered = true;
+        target.power(true);
+        SwitchMeshes();
+    }
+
+    void Off()
+    {
+        powered = false;
+        target.power(false);
+        SwitchMeshes();
+    }
+}
---
+++ Assets/Scripts/Player/Abilities/LeverPuller.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeverPuller : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Lever lever = collider.gameObject.gameObject.GetComponent<Lever>();
+        if (lever != null)
+        {
+            lever.Flip();
+        }
+    }
+}
